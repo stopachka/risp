ceb015a12f616fbe9ad0e4cb38f064186fdf5ed3
stopachka <stepan.p@gmail.com>
2019-04-03 20:24:45 +0400

    repl

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
 use std::io;
-use std::cmp::Ordering;
 
 fn main() {
-  let secret_number = 42;
-
   loop {
-    println!("Guess the number!");
-
-    let mut input_str = String::new();
-    io::stdin().read_line(&mut input_str)
-      .expect("Failed to read string");
+    println!("risp >");
     
-    let guess: u32 = input_str.trim().parse()
-      .expect("Please type a number!");
+    let mut expr = String::new();
+    
+    io::stdin().read_line(&mut expr)
+      .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("too big!"),
-        Ordering::Equal => {
-          println!("you got it!");
-          break;
-        }
-    };
+    print!("=> {}", expr);
   }
 }
