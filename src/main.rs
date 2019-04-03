7a9948a5c8e3ccc21e55c53ee283c83cd928b836
stopachka <stepan.p@gmail.com>
2019-04-03 19:42:13 +0400

    first, guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+  let secret_number = 42;
+
+  loop {
+    println!("Guess the number!");
+
+    let mut input_str = String::new();
+    io::stdin().read_line(&mut input_str)
+      .expect("Failed to read string");
+    
+    let guess: u32 = input_str.trim().parse()
+      .expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small!"),
+        Ordering::Greater => println!("too big!"),
+        Ordering::Equal => {
+          println!("you got it!");
+          break;
+        }
+    };
+  }
 }
