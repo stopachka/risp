11e7b315c705aae4c5928d7326fdf3558202cbd5
stopachka <stepan.p@gmail.com>
2019-04-03 21:02:13 +0400

    tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use std::io;
+extern crate regex;
+use regex::Regex;
+
+fn tokenize(str: &str) -> Vec<String> {
+  let token_regex = Regex::new(
+    r###"[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]+)"###
+  ).unwrap();
+  let mut res = vec![];
+  for cap in token_regex.captures_iter(str) {
+    res.push(String::from(&cap[1]));
+  }
+  res
+}
 
 fn main() {
   loop {
@@ -9,6 +22,6 @@ fn main() {
     io::stdin().read_line(&mut expr)
       .expect("Failed to read line");
 
-    print!("=> {}", expr);
+    print!("=> {}", tokenize(&expr).join(","));
   }
 }
