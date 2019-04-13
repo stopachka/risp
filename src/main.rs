3934b0a1df7291c8cda75d1b9edba4add792840c
stopachka <stepan.p@gmail.com>
2019-04-13 15:45:32 +0400

    tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,95 @@
 use std::io;
-extern crate regex;
-use regex::Regex;
 
-fn tokenize(str: &str) -> Vec<String> {
-  let token_regex = Regex::new(
-    r###"[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]+)"###
-  ).unwrap();
-  let mut res = vec![];
-  for cap in token_regex.captures_iter(str) {
-    res.push(String::from(&cap[1]));
-  }
-  res
+enum RispAtom {
+  Symbol(String),
+  Number(f64),
+}
+
+enum RispExp {
+  List(Vec<RispExp>),
+  Atom(RispAtom),
+}
+
+struct RispErr {
+  reason: &'static str,
+}
+
+/*
+def eval(x: Exp, env=global_env) -> Exp:
+    "Evaluate an expression in an environment."
+    if isinstance(x, Symbol):        # variable reference
+        return env[x]
+    elif isinstance(x, Number):      # constant number
+        return x                
+    elif x[0] == 'if':               # conditional
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':           # definition
+        (_, symbol, exp) = x
+        env[symbol] = eval(exp, env)
+    else:                            # procedure call
+        proc = eval(x[0], env)
+        args = [eval(arg, env) for arg in x[1:]]
+        return proc(*args)
+
+def parse(program: str) -> Exp:
+    "Read a Scheme expression from a string."
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens: list) -> Exp:
+    "Read an expression from a sequence of tokens."
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0) # pop off ')'
+        return L
+    elif token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+def atom(token: str) -> Atom:
+    "Numbers become numbers; every other token is a symbol."
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError:
+            return Symbol(token)
+*/
+
+
+
+fn parse(tokens: Vec<String>) -> Result<RispExp, RispErr> {
+    return Ok(RispExp::Atom(RispAtom::Number(1.0)));
+}
+
+fn tokenize(expr: String) -> Vec<String> {
+  return expr
+    .replace("(", " ( ")
+    .replace(")", " ) ")
+    .split(" ")
+    .map(|x| x.to_string())
+    .collect();
+}
+
+fn slurp_expr() -> String {
+  let mut expr = String::new();
+  
+  io::stdin().read_line(&mut expr)
+    .expect("Failed to read line");
+
+  return expr;
 }
 
 fn main() {
   loop {
     println!("risp >");
-    
-    let mut expr = String::new();
-    
-    io::stdin().read_line(&mut expr)
-      .expect("Failed to read line");
-
-    print!("=> {}", tokenize(&expr).join(","));
+    let _res = parse(
+      tokenize(slurp_expr()),
+    );
   }
 }
