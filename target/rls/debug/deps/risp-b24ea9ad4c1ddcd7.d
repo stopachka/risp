2185dca96e7f15892347a77d2b501461bf6e0c0b
stopachka <stepan.p@gmail.com>
2019-04-27 19:48:42 +0400

    sm

--- target/rls/debug/deps/risp-b24ea9ad4c1ddcd7.d
+++
@@ -1,5 +0,0 @@
-/Users/stopa/Projects/risp/target/rls/debug/deps/risp-b24ea9ad4c1ddcd7.rmeta: src/main.rs
-
-/Users/stopa/Projects/risp/target/rls/debug/deps/risp-b24ea9ad4c1ddcd7.d: src/main.rs
-
-src/main.rs:
