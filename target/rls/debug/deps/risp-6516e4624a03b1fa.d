2eb42c1963c55eba750a497462e0a14aa912f7d8
stopachka <stepan.p@gmail.com>
2019-05-05 17:22:02 -0700

    sm

--- target/rls/debug/deps/risp-6516e4624a03b1fa.d
+++
@@ -1,5 +0,0 @@
-/Users/stopa/Projects/risp/target/rls/debug/deps/risp-6516e4624a03b1fa.rmeta: src/main.rs
-
-/Users/stopa/Projects/risp/target/rls/debug/deps/risp-6516e4624a03b1fa.d: src/main.rs
-
-src/main.rs:
